be7621365be829df55d119746bd3e26a98c4a083
Evgeny Vinnik <evgenyvinnik@gmail.com>
2026-08-11 20:17:24 -0700

    Update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.abiq7jej0bo"
+    "revision": "0.r9kvbjas1go"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
